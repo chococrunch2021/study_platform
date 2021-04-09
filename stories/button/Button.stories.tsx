67fc2f04aa3fd6062422387aa6dd93143831609f
haerangChoi <gofkdvjvl@naver.com>
2021-04-09 23:40:05 +0900

    feat: tsconfig.json 절대 경로 설정

--- stories/button/Button.stories.tsx
+++ stories/button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { Button, ButtonProps } from './Button';
+import { Button, ButtonProps } from 'stories/button/Button';
 
 export default {
   title: 'Component/Button',
