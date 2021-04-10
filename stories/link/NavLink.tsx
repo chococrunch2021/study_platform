2f6dfee37b8ed4ec266e31d15208ca2710991678
haerangChoi <gofkdvjvl@naver.com>
2021-04-10 23:40:43 +0900

    feat: navLink story coponent생성

--- stories/link/NavLink.tsx
+++ stories/link/NavLink.tsx
@@ -1,14 +1,37 @@
 import Link from 'next/link'
-
+import { css } from '@emotion/react'
+import {fontTheme} from '@story/theme/theme'
 export interface NavLinkProps{
     url:string;
-    font?:string;
+    font?:'montserrat'|'almendra'|'nanumgothic';
+    label:string;
+    size?:number;
+    color?:string;
 }
+const link=css`
+font-family: 'Almendra', serif;
+`;
 
-export const NavLink = ({url="/",font="Montserrat"}) => {
+export const NavLink = ({
+    url="/",
+    font="montserrat",
+    label,
+    size=25,
+    color,
+    ...props
+}:NavLinkProps) => {
     return (
-        <Link href={`${url}`}>
-            
+        <Link 
+        href={`${url}`}
+        {...props}>
+            <a 
+            css={[fontTheme[font]]}
+            style={{fontSize:`${size}px`,color}}
+             >{label}</a>
         </Link>
     )
+}
+
+NavLink.defaultProps={
+    color:"#000"
 }
