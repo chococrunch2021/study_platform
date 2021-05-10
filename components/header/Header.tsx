4b1636ba36d8492cf18f7f68d4134a1aee8df669
haerangChoi <gofkdvjvl@naver.com>
2021-05-10 15:25:01 +0900

    feat: header 컴포넌트 오류 수정

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -9,35 +9,32 @@ const flexStyles=css`
     align-items:center;
 `;
 
-
 const LinkBox=styled.li`
     ${flexStyles};
     width:50vw;
+    margin-left:auto;
     justify-content:space-evenly;
+    &> a{
+        color:#fff;
+        text-decoration:none;
+    }
     &>a:hover{
         text-decoration:underline;
     }
+    
 `;
-
-
-export interface HeaderProps {
-
-}
-
 
 export const Header = ({
     ...props
-  }:HeaderProps) => {
+  }) => {
 
     return (
-      <Header {...props}>
-          <LinkBox>
+          <LinkBox {...props}>
             <Link href="/">HOME</Link>
             <Link href="/login">LOG IN</Link>
             <Link href="/about">우쥬스터디란?</Link>
             <Link href="/contact">CONTACT US</Link>
           </LinkBox>
-      </Header>
     );
   };
 
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,38 +1,13 @@
 import React from 'react'
 import { Button } from 'components/button/Button'
+import { Header } from 'components/header/Header';
 import Image from 'next/image';
 import { css } from '@emotion/react';
-import styled from '@emotion/styled'
-
-import Link from 'next/link';
-
-const flexStyles=css`
-    width:100%;
-    display:flex;
-    align-items:center;
-`;
-
-const LinkBox=styled.li`
-    ${flexStyles};
-    width:50vw;
-    margin-left:auto;
-    justify-content:space-evenly;
-    &> a{
-        color:#fff;
-        text-decoration:none;
-    }
-    &>a:hover{
-        text-decoration:underline;
-    }
-    
-`;
-
-
 
 const landingImage = css`
     z-index:0;
 `;
-const buttonStyle = css`
+const elementStyle = css`
     z-index:1;
     position:relative;
 `;
@@ -48,15 +23,9 @@ function Index() {
                 objectFit="cover"
                 objectPosition="center"
             ></Image>
-            <section css={buttonStyle}>
-                <LinkBox>
-                <Link href="/">HOME</Link>
-                <Link href="/login">LOG IN</Link>
-                <Link href="/about">우쥬스터디란?</Link>
-                <Link href="/contact">CONTACT US</Link>
-                </LinkBox>
-                <Button css={buttonStyle} label="test" primary></Button>
-                <div style={{ color:"#fff"}}>test</div>
+            {/* 배경 아닌 컴포넌트들 => element */}
+            <section css={elementStyle}>
+                <Header/>
             </section>
             </div>
           
