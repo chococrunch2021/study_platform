47e74a7e8d2ecd5e56ea5fd4a8a83146d3facaa5
haerangChoi <gofkdvjvl@naver.com>
2021-05-10 14:14:13 +0900

    feat: next-image로 홈 배경이미지 적용

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import { Button } from 'components/button/Button'
+import Image from 'next/image';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled'
+
+const landingImage = css`
+    z-index:0;
+`;
+
+
 function Index() {
     return (
-        <Button label="test"/>
+        <div>
+            <Image
+                css={landingImage}
+                src="/static/galaxy-bg.svg"
+                alt="galaxy-bg"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+            ></Image>
+               
+               
+            </div>
     )
 }
 
